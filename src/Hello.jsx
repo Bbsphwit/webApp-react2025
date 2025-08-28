ecf8ba7d73df4a02783f54f2509b9c1a8ca2656a
sophonwit.t <120322211+Bbsphwit@users.noreply.github.com>
2025-08-28 08:22:49 +0000

    Update title and add Hello component to the application

---
+++ src/Hello.jsx
@@ -0,0 +1,10 @@
+export default function Hello() {
+    let name = "Sophonwit Thapseng"
+    return (<>
+        <div>
+            <h1>Hello  {"Mr." + name.toUpperCase()}</h1>
+            <hr></hr>
+        </div>
+    </>
+    );
+};
