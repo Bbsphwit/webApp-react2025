3ab63a3d2e726844b022dfa15ee8106bdde0d50e
sophonwit.t <120322211+Bbsphwit@users.noreply.github.com>
2025-09-11 08:45:41 +0000

    Add List component and styling; refactor Count component rendering

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,19 +1,47 @@
-export default function Count() {
-    const [count, setCount] = React.useState(0);
-    const [value, setValue] = React.useState('0');
-    return (<>
-        <div>
-            <p>Type cm.</p>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-            <p>{parseFloat(value)} cm. = {parseFloat(value / 10).toFixed(2)} m.</p>
-            <p>{parseFloat(value)} cm. = {parseFloat(value * 0.393701).toFixed(2)} in.</p>
+import React from "react";
 
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>Increase</button>
-            <button onClick={() => setCount(count - 1)}>Decrease</button>
-            <button onClick={() => setCount(0)}>Clear</button>
-
-        </div>
-    </>
+function Item({ name, isPacket }) {
+  if (isPacket) {
+    return (
+      <li>
+        {" "}
+        {name} {isPacket && "✅"}
+      </li>
     );
-};
+  }
+}
+
+export default function Count() {
+  const [count, setCount] = React.useState(0);
+  const [value, setValue] = React.useState("0");
+
+  const numValue = parseFloat(value) || 0;
+
+  return (
+    <div>
+      <ol>
+        <Item name="flashlight" isPacket={true} />
+        <Item name="sunglasses" isPacket={false} />
+        <Item name="towel" isPacket={false} />
+      </ol>
+
+      <p>Type cm.</p>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <p>
+        {numValue} cm. = {(numValue / 100).toFixed(2)} m.
+      </p>
+      <p>
+        {numValue} cm. = {(numValue * 0.393701).toFixed(2)} in.
+      </p>
+
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Clear</button>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Count from "./Count";
+import List from "./List";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Count />
+    {/* <App /> */}
+    {/* <Count /> */}
+    <List />
   </React.StrictMode>
 );
 
